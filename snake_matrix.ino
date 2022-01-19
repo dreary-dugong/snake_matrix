b118a129eb1f366aa3dffbd2f87e8c61c8bf705a
Daniel Gysi <danielgysi@protonmail.com>
2022-01-19 17:07:44 -0500

    fixed pointer issues. I understand now.

--- snake_matrix.ino
+++ snake_matrix.ino
@@ -15,33 +15,35 @@ struct Food{
 };
 
 //input pins to control direction
-const int RIGHT_PIN = 6;
-const int LEFT_PIN = 7;
+const int RIGHT_PIN = 7;
+const int LEFT_PIN = 6;
 
 //our LED matrix
 Adafruit_IS31FL3731 matrix = Adafruit_IS31FL3731();
 
 //game variables
 int snakeLength;
-Segment head;
-Segment tail;
+Segment *head_ptr;
+Segment *tail_ptr;
 Food currFood = {0,0};
 bool gameOver = false;
 
 void setup() {
   //initialize our matrix. We assume this won't fail. 
   matrix.begin();
-  matrix.setRotation(0); //this may need to be changed
+  matrix.setRotation(1); //this may need to be changed
 
   /*
     Game setup
   */
   //initial snake
-  head = {6, 5, NULL};
+  Segment head = {6, 5, NULL};
   Segment mid = {5, 5, &head};
-  tail = {4, 5, &mid};
+  Segment tail = {4, 5, &mid};
+  head_ptr = &head;
+  tail_ptr = &tail;
+
   lightSnake();
-  
   createFood();
 
 }
@@ -58,32 +60,32 @@ void loop() {
   
     //new head in the new direction
     if(left and not right){
-      newhead = {head.x-1, head.y, &head};
+      newhead = {head_ptr->x-1, head_ptr->y, NULL};
     } else if(right and not left){
-      newhead = {head.x+1, head.y, &head};
+      newhead = {head_ptr->x+1, head_ptr->y, NULL};
     } else if(right and left){
-      newhead = {head.x, head.y-1, &head};
+      newhead = {head_ptr->x, head_ptr->y-1, NULL};
     } else {
-      newhead = {head.x, head.y+1, &head};
+      newhead = {head_ptr->x, head_ptr->y+1, NULL};
     }
 
-    head = newhead;
+    head_ptr->next = &newhead;
+    head_ptr = &newhead;
    
-    if isCollision(){
+    if(isCollision()){
       gameOver = true;
-      continue;
     }
     
     //if the new head is on food, don't decrease length
-    if (not(head.x == currFood.x and head.y == currFood.y)){
-      tail = *tail.next;
-    } else {
+    if (head_ptr->x == currFood.x and head_ptr->y == currFood.y){
       snakeLength += 1;
       createFood();
+    } else {
+      tail_ptr = tail_ptr->next;
     }
   
     lightSnake();
-    delay(500);
+    delay(1000);
   }
 }
 
@@ -95,10 +97,10 @@ void gameOverDisp(){
 void lightSnake(){
   
   //use full brightness for now until we're sure it works
-  Segment currSeg = tail;
+  Segment *currSeg = tail_ptr;
   for(int i=0; i<snakeLength; i++){
-    matrix.drawPixel(currSeg.x, currSeg.y, 255);
-    currSeg = *currSeg.next;
+    matrix.drawPixel(currSeg->x, currSeg->y, 255);
+    currSeg = currSeg->next;
   }
   
 }
@@ -106,19 +108,21 @@ void lightSnake(){
 //randomly choose a pixel that isn't in the snake
 //asign that to food and light it. No blinking yet. 
 void createFood(){
+
   bool goodPoint = false;
   int x;
   int y;
+
   while(not goodPoint){
     x = random(0, 9);
     y = random(0, 16);
     goodPoint = true;
-    Segment currSegment = tail;
+    Segment *currSegment = tail_ptr;
     for(int i=0; i<snakeLength; i++){  
-      if(currSegment.x == x and currSegment.y == y){
+      if(currSegment->x == x and currSegment->y == y){
         goodPoint = false;
       }
-    currSegment = *currSegment.next; 
+    currSegment = currSegment->next; 
     }
   }     
   
@@ -129,18 +133,18 @@ void createFood(){
 }
 
 //detect if the snake's head collided with a wall or its body
-void isCollision(){
-  if(head.x < 0 or head.x > 8 or head.y <  0 or head.y > 15){
+bool isCollision(){
+  if(head_ptr->x < 0 or head_ptr->x > 8 or head_ptr->y < 0 or head_ptr->y > 15){
     return true;
   }
  
-  Segment currSeg = tail; 
+  Segment *currSeg = tail_ptr; 
 
   for(int i=0; i<snakeLength; i++){
-    if(currSeg.x == head.x and currSeg.y == head.y){
+    if(currSeg->x == head_ptr->x and currSeg->y == head_ptr->y){
        return true;
     }
-    currSeg = *currSeg.next;
+    currSeg = currSeg->next;
   }
 
   return false;
